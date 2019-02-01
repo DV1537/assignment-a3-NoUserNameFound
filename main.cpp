55e5f3c568ad12f0d1a4d63691729b7040a9c053
Daniel Nygren <daqqqe@gmail.com>
2019-02-01 19:47:04 +0100

    finish

--- main.cpp
+++ main.cpp
@@ -1,33 +1,69 @@
-#include <iostream>
+#include<iostream>
 #include <fstream>
+#include<string>
 
-/**
- * The program reads numbers from a file and prints the sum of them
- * Example: main.exe ./tests/input.in
- * @param  argc [description]
- * @param  argv [description]
- * @return      [description]
- */
+using namespace std;
+
 int main(int argc, const char * argv[])
 {
-    
-    int a = 0;
-    int sum = 0;
-    
-    std::ifstream myReadFile;
-    
-    myReadFile.open(argv[1]);
-    
-    while (myReadFile >> a)
-    {
-        sum += a;
-    }
-    myReadFile.close();
-    
-    
-    
-    std::cout << sum << "\n";
-    
-    return 0;
-}
+	int x;
+	int sum = 0;
+	int size = 2;
+	int number = 0;
+	int* numbers = new int[size];
 
+	ifstream inFile;
+	inFile.open(argv[1]);
+
+	if (!inFile)
+	{
+		cout << "Could not open file!" << endl;
+	}
+	else
+	{
+		cout << "Input: ";
+		while (inFile >> x)
+		{
+			if (number<size)
+			{
+				sum += x;
+				numbers[number] = x;
+				number++;
+			}
+			else
+			{
+				int* temp = new int[size];
+				for (int i = 0; i < size; i++)
+				{
+					temp[i] = numbers[i];
+				}
+				delete[] numbers;
+				numbers = new int[++size];
+				for (int i = 0; i < size - 1; i++)
+				{
+					numbers[i] = temp[i];
+				}
+				delete[] temp;
+
+				sum += x;
+				numbers[number++] = x;
+			}
+			cout << to_string(x) << " ";
+		}
+		int avarage = sum / size;
+		cout << "          Output: ";
+		for (int i = 0; i < size; i++)
+		{
+			if (numbers[i] > avarage)
+			{
+				cout << to_string(numbers[i]) << " ";
+			}
+
+		}
+		cout << endl;
+	}
+
+	system("pause");
+	delete[] numbers;
+	return 0;
+}
